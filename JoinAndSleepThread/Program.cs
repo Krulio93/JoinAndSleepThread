2762847ccddf58d6dd180330b2ed96b37682b82b
Krulio93 <matthias@arimba.de>
2018-10-25 16:27:44 +0200

    Projektdateien hinzufügen.

---
+++ JoinAndSleepThread/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JoinAndSleepThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Go);
+            t.Start();
+            t.Join();
+
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+        }
+
+        static void Go()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+
+    }
+}
